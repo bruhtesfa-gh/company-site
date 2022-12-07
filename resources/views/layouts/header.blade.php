57c810b33b92ed80549d9b02753b48d4e6623308
Yosef781227 <yosefnakachew101@gmail.com>
2022-12-07 12:03:49 +0300

    edited nav bar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,7 +2,7 @@
 <header id="header" class="single-page-header header d-flex align-items-center">
     <div class="container container-xl d-flex align-items-center justify-content-between">
         <a href="index.html" class="logo d-flex align-items-center">
-            <h1><span>Eu</span>soft<span>.</span></h1>
+            <h1><span>Fe</span>jlesztő<span>.</span></h1>
         </a>
         <nav id="navbar" class="navbar">
             <ul>
@@ -16,21 +16,21 @@
                     </ul>
                 </li>
                 <li><a href="about.html">About</a></li>
-                <li><a href="packages.html">Pricing</a></li>
+                <!-- <li><a href="packages.html">Pricing</a></li> -->
                 <li><a href="testimonials.html">Testimonials</a></li>
                 <li class="dropdown"><a href="#"><span>All Pages</span> <i
                             class="bi bi-chevron-down dropdown-indicator"></i></a>
                     <ul>
-                        <li><a href="login.html">Login</a></li>
-                        <li><a href="register.html">Signup</a></li>
+                        <!-- <li><a href="login.html">Login</a></li> -->
+                        <!-- <li><a href="register.html">Signup</a></li> -->
                         <li><a href="faq.html">FAQs</a></li>
                         <li><a href="team.html">Team</a></li>
                         <li><a href="blogs.html">Blogs</a></li>
-                        <li><a href="blogs-details.html">Blog Details</a></li>
-                        <li><a href="coming-soon.html">Coming Soon</a></li>
+                        <!-- <li><a href="blogs-details.html">Blog Details</a></li> -->
+                        <!-- <li><a href="coming-soon.html">Coming Soon</a></li> -->
                         <li><a href="terms.html">Terms & Conditions</a></li>
                         <li><a href="policy.html">Privacy Policy</a></li>
-                        <li><a href="sample.html">Sample Page</a></li>
+                        <!-- <li><a href="sample.html">Sample Page</a></li> -->
                     </ul>
                 </li>
                 <li><a href="blogs.html">Blogs</a></li>
