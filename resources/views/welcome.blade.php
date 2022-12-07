9ef1c006ae5496c34834f766fd38cc98603fa44f
Yosef781227 <yosefnakachew101@gmail.com>
2022-12-07 15:07:17 +0300

    All services info

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,9 +61,14 @@
             <div class="col-md-4 col-sm-12 equal-height">
                 <div class="item">
                     <i class="fa fa-tasks"></i>
-                    <h4>Project Management</h4>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    <h4>Project Content Management</h4>
+                    <p align="justify">
+                    A Content Management System is a web application that makes
+content authoring and content delivery easy. It enables even non-
+technical users to efficiently build a website, streamline the web
+publishing procedures and quickly deploy them, creating a content-rich
+website.
+.
                     </p>
                 </div>
             </div>
@@ -73,9 +78,13 @@
             <div class="col-md-4 col-sm-12 equal-height">
                 <div class="item">
                     <i class="fa fa-tachometer"></i>
-                    <h4>Project Impliment</h4>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    <h4>UI/UX Design</h4>
+                    <p p align="justify">
+                    Intuitive designs combined with compelling user experience are what makes our
+apps stand ahead in the digital age. Seamless experience driven by international
+standards in collaboration with out-of-the-box ideas is the specialty of Fejlesztő 
+helping your business accomplish its goals.
+
                     </p>
                 </div>
             </div>
@@ -85,9 +94,12 @@
             <div class="col-md-4 col-sm-12 equal-height">
                 <div class="item">
                     <i class="fa fa-recycle"></i>
-                    <h4>Software Update</h4>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    <h4>Software Update Services</h4>
+                    <p align="justify">
+                    Updates can prevent security issues and improve compatibility and program features. 
+                    Software updates are necessary to keep computers, mobile devices and tablets running
+                     smoothly -- and they may lower security vulnerabilities.
+                     Data breaches, hacks, cyber attacks and identity theft have all been in the news.
                     </p>
                 </div>
             </div>
@@ -98,8 +110,11 @@
                 <div class="item">
                     <i class="fa fa-handshake-o"></i>
                     <h4>24/7 Support</h4>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    <p align="justify">
+                    Whether for on-site assistance, technical support, or remote support, Source is here to ensure 
+                    our customers’ success and end-users’ business productivity. We offer dependable, on-demand
+                     support options including 24/7 technical and remote support
+                     and 4-hour and next business day global on-site support in over 100 countries worldwide
                     </p>
                 </div>
             </div>
