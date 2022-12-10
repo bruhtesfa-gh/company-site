2493dc212a9cb4c94b2a8ede76e2110b3b82abef
bruhtesfa-gh <bruhtesfazelealem436@gmail.com>
2022-12-10 12:33:29 +0300

    sayzaba push

--- resources/views/inclusive-team.blade.php
+++ resources/views/inclusive-team.blade.php
@@ -2,7 +2,7 @@
     @foreach ($teams as $team)
     <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="100">
         <div class="member">
-            <img src="{{"http://127.0.0.1:8000/" . $team->image}}" class="img-team" alt="">
+            <img src="{{"http://127.0.0.1:8000/" . $team->image}}" class="img-fluid" alt="">
             <div class="social">
                 @foreach ($team->contact_adds as $social)
                 <a href="{{$social->url}}"><i class="{{"bi bi-" . $social->media}}"></i></a>
