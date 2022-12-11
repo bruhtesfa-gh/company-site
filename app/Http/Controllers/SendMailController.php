8b411282d3251071285569cef2eed30bdcf6526c
bruhtesfa-gh <bruhtesfazelealem436@gmail.com>
2022-12-11 16:14:39 +0300

    url changed

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -44,11 +44,11 @@ class SendMailController extends Controller
                 $mail->Host       = 'smtp.titan.email';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                 $mail->Username   = 'noreply@fejleszto-tech.com';                     //SMTP username
-                $mail->Password   = '';                               //SMTP password
+                $mail->Password   = 'tIyuKdLUX8';                               //SMTP password
                 $mail->SMTPSecure = 'tsl';            //Enable implicit TLS encryption
                 $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-                $mail->setFrom('noreply@fejleszto-tech.com', '');
+                $mail->setFrom('noreply@fejleszto-tech.com', 'Fejleszto');
 
                 $mail->addAddress($email, $name);
                 //Add a recipient
@@ -58,7 +58,7 @@ class SendMailController extends Controller
 
                 $_message = '<div style="width: 100%; display: flex; justify-content: center;">
                                 <div style="width: 720px;">
-                                    <img src="http://127.0.0.1:8000/assets/logo/fejleszto-capp-trasparent.png" alt="">
+                                    <img src="http://fejleszto-tech.com/assets/logo/fejleszto-capp-trasparent.png" alt="">
                                     <div>
                                         <h3 style="color: dodgerblue; font-size: 3rem; margin: 0; padding: 0;">Technology</h3>
                                         <hr>
