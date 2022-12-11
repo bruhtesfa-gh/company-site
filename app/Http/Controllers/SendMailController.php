e85a100ed55b863a383115bd8461385da2b390b0
bruhtesfa-gh <bruhtesfazelealem436@gmail.com>
2022-12-11 15:58:10 +0300

    mail configuration

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -43,23 +43,48 @@ class SendMailController extends Controller
                 $mail->isSMTP();                                            //Send using SMTP
                 $mail->Host       = 'smtp.titan.email';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                $mail->Username   = 'development@ewea-ercs.org';                     //SMTP username
-                $mail->Password   = 'tIyuKdLUX8';                               //SMTP password
+                $mail->Username   = 'noreply@fejleszto-tech.com';                     //SMTP username
+                $mail->Password   = '';                               //SMTP password
                 $mail->SMTPSecure = 'tsl';            //Enable implicit TLS encryption
                 $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-                $mail->setFrom('development@ewea-ercs.org', '');
+                $mail->setFrom('noreply@fejleszto-tech.com', '');
 
                 $mail->addAddress($email, $name);
                 //Add a recipient
                 // $mail->addAddress('ellen@example.com');               //Name is optional
-                $mail->addReplyTo('development@ewea-ercs.org', 'For More Information');
-                $mail->addCC('development@ewea-ercs.org');
+                $mail->addReplyTo('support@fejleszto-tech.com', 'For More Information');
+                $mail->addCC('noreply@fejleszto-tech.com');
 
-                //Content
+                $_message = '<div style="width: 100%; display: flex; justify-content: center;">
+                                <div style="width: 720px;">
+                                    <img src="http://127.0.0.1:8000/assets/logo/fejleszto-capp-trasparent.png" alt="">
+                                    <div>
+                                        <h3 style="color: dodgerblue; font-size: 3rem; margin: 0; padding: 0;">Technology</h3>
+                                        <hr>
+                                    </div>
+                                    <div>
+                                        <p>
+                                            Hello, ' . $name . '
+                                        </p>
+                                        <p>
+                                            Thank you for contacting the Fejlesztő Technologies. This is an automated message to notify you that
+                                            we recieved your massage. We will contact you as soon as possible in a few hours
+                                        </p>
+                                        <hr>
+                                        <p>
+                                            Fejlesztő Technologies , 04 Giorgis Rd Frindship building 3rd Floor
+                                            <br>
+                                            Bahir Dar, Ethiopia
+                                            <br>
+                                            <a href="mailto:support@fejleszto-tech.com">support@fejleszto-tech.com</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>';
                 $mail->isHTML(true);                                  //Set email format to HTML
                 $mail->Subject = $subject;
-                $mail->Body    = "*This is a system generated email - please do not reply* <br> Dear " . $name . " <br> " . $subject;
+                $mail->Body    = $_message;
                 //$mail->AltBody = 'go to ' . $this->link;
                 if ($mail->Send()) {
                     return true;
